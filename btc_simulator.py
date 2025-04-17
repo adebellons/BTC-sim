75c6117e9b3023e054e13601e7c3c26dd1d97b1f
adebellons <kevin@kdebello.com>
2025-04-17 13:02:29 -0400

    Update btc_simulator.py

--- btc_simulator.py
+++ btc_simulator.py
@@ -129,8 +129,8 @@ if run_sim:
 
             # Update and record all active loans
             for loan in active_loans:
-                if m >= loan['start_month']:
-                    num_active_loans = len([l for l in active_loans if m >= l['start_month']])
+                if m > loan['start_month']:  # skip first month of each loan
+                    num_active_loans = len([l for l in active_loans if m > l['start_month']])
                     monthly_share_payment = payment / num_active_loans if num_active_loans > 0 else 0.0
 
                     interest = loan['loan_balance'] * monthly_interest_rate
